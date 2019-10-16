758f3357cbe04b3170fd44104ddcd12d439c8b5c
Ryan Newman <Ryan.N.Newman@Outlook.com>
2019-10-16 11:25:13 -0400

    Added an About Page and updated it with HTML/CSS

---
+++ MVC/Mud/Mud/Models/ViewModels/DungeonListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mud.Models.ViewModels
+{
+    public class DungeonListViewModel
+    {
+        //public IEnumerable<Dungeon> Dungeon { get; set; }
+        //public PagingInfo PagingInfo { get; set; }
+        //public string CurrentCategory { get; set; }
+    }
+}
