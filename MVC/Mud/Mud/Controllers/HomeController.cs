c0fab94ec1b5a7afd20d6e3e6b75b69bef896985
Ryan Newman <Ryan.N.Newman@Outlook.com>
2019-10-24 09:07:26 -0400

    updated Controller for PVE

--- MVC/Mud/Mud/Controllers/HomeController.cs
+++ MVC/Mud/Mud/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace Mud.Controllers
         {
             return View();
         }
+        public IActionResult PVE()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
